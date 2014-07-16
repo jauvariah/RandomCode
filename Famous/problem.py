af9e99eb95b1530dff0dffd64fd2282bad8cdf9f
Tom Sun <tomsun100@gmail.com>
2014-07-16 17:17:11 -0400

    Added Famo.us coding challenge.

---
+++ Famous/problem.py
@@ -0,0 +1,10 @@
+a = "12345"
+b = "67890"
+
+def string_add(a, b):
+    outputNum = int(float(a)) + int(float(b))
+    return str(outputNum)
+
+print string_add("1", "2")
+print string_add("9", "9")
+print string_add(a, b)
